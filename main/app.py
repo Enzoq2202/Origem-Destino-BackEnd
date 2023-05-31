d720368b538b92a42fa4bf7a770a92581e3e4c4c
Enzo <enzoq@al.insper.edu.br>
2023-05-31 15:38:15 -0300

    back

--- main/app.py
+++ main/app.py
@@ -5,15 +5,87 @@ from pathlib import Path
 
 app = Flask(__name__)
 
-# #Obtendo Path do DB
-# FILE = Path(__file__).resolve()
-# src_folder = FILE.parents[0]
-# rel_arquivo_db = Path('db/cadastro.db')
-# db = Path(src_folder / rel_arquivo_db).resolve()
+#Obtendo Path do DB
+FILE = Path(__file__).resolve()
+src_folder = FILE.parents[0]
+rel_arquivo_db = Path('db/maps.db')
+db = Path(src_folder / rel_arquivo_db).resolve()
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+
+@app.route('/rota', methods=['POST'])
+def rota():
+    #Obtendo dados do POST
+    data = request.get_json()
+    print(data)
+
+    #Criando a conexão com o banco de dados
+    conn = sqlite3.connect(db)
+
+    #Inserindo dados na tabela
+    conn.execute('''INSERT INTO MinhaTabela (
+        LatitudeOrigem,
+        LongitudeOrigem,
+        LatitudeDestino,
+        LongitudeDestino,
+        TravelMode,
+        EncodedRoutes,
+        DistanceMeters,
+        Duration
+    ) VALUES (?,?,?,?,?,?,?,?)''', (
+        data['LatitudeOrigem'],
+        data['LongitudeOrigem'],
+        data['LatitudeDestino'],
+        data['LongitudeDestino'],
+        data['TravelMode'],
+        data['EncodedRoutes'],
+        data['DistanceMeters'],
+        data['Duration']
+    ))
+
+    #Salva as alterações
+    conn.commit()
+
+    #Fecha a conexão
+    conn.close()
+
+    return "Rota adicionada com sucesso!"
+
+@app.route('/rota', methods=['GET'])
+def rotas():
+    #Criando a conexão com o banco de dados
+    conn = sqlite3.connect(db)
+
+    #Obtendo dados da tabela
+    cursor = conn.execute('''SELECT * FROM MinhaTabela''')
+
+    #Criando lista de rotas
+    rotas = []
+
+    #Percorrendo dados da tabela
+    for row in cursor:
+        #Criando dicionário de rota
+        rota = {
+            'id': row[0],
+            'latitudeOrigem': row[1],
+            'longitudeOrigem': row[2],
+            'latitudeDestino': row[3],
+            'longitudeDestino': row[4],
+            'travelMode': row[5],
+            'encodedRoutes': row[6],
+            'distanceMeters': row[7],
+            'duration': row[8]
+        }
+
+        #Adicionando rota na lista de rotas
+        rotas.append(rota)
+
+    #Fecha a conexão
+    conn.close()
+
+    #Retorna lista de rotas
+    return {'rotas': rotas}
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main/db/script.py
+++ main/db/script.py
@@ -4,6 +4,21 @@ import sqlite3
 # Criando a conexão com o banco de dados
 conn = sqlite3.connect('maps.db')
 
+conn.execute('''CREATE TABLE MinhaTabela (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    LatitudeOrigem REAL,
+    LongitudeOrigem REAL,
+    LatitudeDestino REAL,
+    LongitudeDestino REAL,
+    TravelMode TEXT,
+    EncodedRoutes TEXT,
+    DistanceMeters REAL,
+    Duration TEXT
+);''')
+
+
+
+
 
 # Salva as alterações
 conn.commit()
